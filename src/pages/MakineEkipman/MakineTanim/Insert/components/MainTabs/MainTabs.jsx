82e361b433d2e887070e75435c8193445d6b8e70
Anar Mamedov <anar26271@gmail.com>
2023-12-20 20:48:14 +0300

    test

--- src/pages/MakineEkipman/MakineTanim/Insert/components/MainTabs/MainTabs.jsx
+++ src/pages/MakineEkipman/MakineTanim/Insert/components/MainTabs/MainTabs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Drawer, Typography, Button, Input, Select, DatePicker, TimePicker, Row, Col } from "antd";
+import { Drawer, Typography, Button, Input, Select, DatePicker, TimePicker, Row, Col, Checkbox } from "antd";
 import { Controller, useFormContext } from "react-hook-form";
 import Location from "./components/Location";
 import MakineTipi from "./components/MakineTipi";
@@ -8,6 +8,7 @@ import MarkaSelect from "./components/MarkaSelect";
 import ModelSelect from "./components/ModelSelect";
 import MakineDurum from "./components/MakineDurum";
 import MasterMakineTablo from "./components/MasterMakineTablo";
+import MakineTakvimtablo from "./components/MakineTakvimtablo";
 
 const { Text, Link } = Typography;
 
@@ -17,6 +18,11 @@ export default function MainTabs() {
   const handleMinusClick = () => {
     setValue("masterMakineTanimi", "");
     setValue("masterMakineID", "");
+  };
+
+  const handleTakvimMinusClick = () => {
+    setValue("makineTakvimTanimi", "");
+    setValue("makineTakvimID", "");
   };
 
   return (
@@ -36,6 +42,8 @@ export default function MainTabs() {
           padding: "5px",
           justifyContent: "center",
           gap: "5px",
+          alignContent: "flex-start",
+          height: "fit-content",
         }}>
         <div
           style={{
@@ -83,6 +91,7 @@ export default function MainTabs() {
           justifyContent: "center",
           gap: "5px",
           alignContent: "flex-start",
+          height: "fit-content",
         }}>
         <MakineDurum />
         <div
@@ -142,11 +151,60 @@ export default function MainTabs() {
             />
             <MasterMakineTablo
               onSubmit={(selectedData) => {
-                setValue("masterMakineTanimi", selectedData.subject);
+                setValue("masterMakineTanimi", selectedData.definition);
                 setValue("masterMakineID", selectedData.key);
               }}
             />
             <Button onClick={handleMinusClick}> - </Button>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}>
+          <Text style={{ fontSize: "14px" }}>Çalışma Takvimi:</Text>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "300px",
+            }}>
+            <Controller
+              name="makineTakvimTanimi"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text" // Set the type to "text" for name input
+                  style={{ width: "215px" }}
+                  disabled
+                />
+              )}
+            />
+            <Controller
+              name="makineTakvimID"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  type="text" // Set the type to "text" for name input
+                  style={{ display: "none" }}
+                />
+              )}
+            />
+            <MakineTakvimtablo
+              onSubmit={(selectedData) => {
+                setValue("makineTakvimTanimi", selectedData.subject);
+                setValue("makineTakvimID", selectedData.key);
+              }}
+            />
+            <Button onClick={handleTakvimMinusClick}> - </Button>
           </div>
         </div>
         <div
@@ -232,6 +290,90 @@ export default function MainTabs() {
             <Text style={{ fontSize: "12px" }}>(saat/yıl)</Text>
           </div>
         </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "column",
+          border: "1px solid #80808068",
+          width: "fit-content",
+          padding: "5px",
+          justifyContent: "flex-start",
+          gap: "15px",
+          alignContent: "flex-start",
+          height: "fit-content",
+        }}>
+        <Controller
+          name="makineAktif"
+          control={control}
+          defaultValue={true} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              Aktif
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="makineKalibrasyon"
+          control={control}
+          defaultValue={false} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              Kalibrasyon Var
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="kritikMakine"
+          control={control}
+          defaultValue={false} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              Kritik Makine
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="makineGucKaynagi"
+          control={control}
+          defaultValue={false} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              Güç Kaynağı
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="makineIsBildirimi"
+          control={control}
+          defaultValue={false} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              İş Bildirimi
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="makineYakitKullanim"
+          control={control}
+          defaultValue={false} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              Yakıt Kullanım
+            </Checkbox>
+          )}
+        />
+        <Controller
+          name="makineOtonomBakim"
+          control={control}
+          defaultValue={false} // or true if you want it checked by default
+          render={({ field }) => (
+            <Checkbox checked={field.value} onChange={(e) => field.onChange(e.target.checked)}>
+              Otonom Bakım
+            </Checkbox>
+          )}
+        />
       </div>
     </div>
   );
