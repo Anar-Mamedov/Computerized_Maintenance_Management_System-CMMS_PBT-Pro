332ef72c4d4350c9ddbc332d63b50007d8e74469
Anar Mamedov <anar26271@gmail.com>
2024-02-08 13:03:17 +0300

    test

--- src/pages/YardimMasasi/IsTalepleri/components/IsEmrineCevir/Teknisyen/TeknisyenSubmit.jsx
+++ src/pages/YardimMasasi/IsTalepleri/components/IsEmrineCevir/Teknisyen/TeknisyenSubmit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Modal } from "antd";
 import { CheckOutlined } from "@ant-design/icons";
 import { Controller, useForm, FormProvider } from "react-hook-form";
@@ -7,6 +7,8 @@ import dayjs from "dayjs";
 import TeknisyenIsEmriCevir from "./TeknisyenIsEmriCevir";
 
 export default function TeknisyenSubmit({ selectedRows, refreshTableData }) {
+  // Butonun disabled durumunu kontrol et
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const methods = useForm({
     defaultValues: {
       personelTanim: "",
@@ -14,7 +16,7 @@ export default function TeknisyenSubmit({ selectedRows, refreshTableData }) {
       // Add other default values here
     },
   });
-  const { setValue, reset, handleSubmit } = methods;
+  const { setValue, reset, handleSubmit, watch } = methods;
 
   const formatDateWithDayjs = (dateString) => {
     const formattedDate = dayjs(dateString);
@@ -25,6 +27,15 @@ export default function TeknisyenSubmit({ selectedRows, refreshTableData }) {
     const formattedTime = dayjs(timeObj);
     return formattedTime.isValid() ? formattedTime.format("HH:mm:ss") : "";
   };
+
+  // personelID değerini izle
+  const personelID = watch("personelID");
+
+  // personelID veya selectedRows değiştiğinde butonun durumunu güncelle
+  useEffect(() => {
+    const tbIsTalepId = selectedRows.map((row) => row.key).join(",");
+    setIsButtonDisabled(!personelID || !tbIsTalepId);
+  }, [personelID, selectedRows]);
 
   const onSubmited = (data) => {
     // Tablodan seçilen kayıtların key değerlerini birleştir
@@ -52,8 +63,18 @@ export default function TeknisyenSubmit({ selectedRows, refreshTableData }) {
         <form style={{ width: "100%" }} onSubmit={methods.handleSubmit(onSubmited)}>
           <TeknisyenIsEmriCevir selectedRows={selectedRows} />
         </form>
-        <Button type="text" onClick={methods.handleSubmit(onSubmited)}>
-          <CheckOutlined />
+        <Button
+          style={{ paddingLeft: "0px" }}
+          type="submit"
+          onClick={methods.handleSubmit(onSubmited)}
+          disabled={isButtonDisabled} // Butonun disabled durumunu ayarla
+        >
+          <CheckOutlined
+            style={{
+              color: isButtonDisabled ? "grey" : "rgb(0, 211, 0)", // Buton disabled ise rengi gri yap
+              fontSize: "18px",
+            }}
+          />
         </Button>
       </div>
     </FormProvider>
