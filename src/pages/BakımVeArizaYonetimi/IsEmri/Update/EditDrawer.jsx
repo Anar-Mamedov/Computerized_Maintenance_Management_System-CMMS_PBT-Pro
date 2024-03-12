3d5baa7e75f741d5098c11cb8281863e86f41c70
Anar Mamedov <anar26271@gmail.com>
2024-03-12 06:40:22 +0300

    Update EditDrawer.jsx

--- src/pages/BakımVeArizaYonetimi/IsEmri/Update/EditDrawer.jsx
+++ src/pages/BakımVeArizaYonetimi/IsEmri/Update/EditDrawer.jsx
@@ -409,6 +409,7 @@ export default function EditDrawer({ selectedRow, onDrawerClose, drawerVisible, 
           );
           setValue("maliyet", item.ISM_MALIYET_DISSERVIS);
           setValue("garantiKapsami", item.GARANTI);
+          setValue("prosedurTipi", item.ISM_TIP_KOD_ID);
 
           // ... Diğer setValue çağrıları
 
