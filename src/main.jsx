d59afe845ec6a3a5929181cd05d23223c7670b17
Anar Mamedov <anar26271@gmail.com>
2024-04-03 16:04:20 +0300

    iş emri kapatma

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,39 @@ import App from "./App.jsx";
 import { ConfigProvider } from "antd";
 import trTR from "antd/es/locale/tr_TR";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AppProvider } from "./AppContext"; // Yolu güncelleyin
+import { AppProvider } from "./AppContext";
 import { RecoilRoot } from "recoil";
+import dayjs from "dayjs"; // Day.js kütüphanesini ekleyin
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ConfigProvider locale={trTR}>
-      <Router>
-        <AppProvider>
-          <RecoilRoot>
-            <App />
-          </RecoilRoot>
-        </AppProvider>
-      </Router>
-    </ConfigProvider>
-  </React.StrictMode>
-);
+// Uygulamanın kullanılamayacağı durum için bir bileşen
+const AppDisabled = () => <div style={{ textAlign: "center", marginTop: "20%" }}></div>;
+
+// Uygulamanın render edilip edilmeyeceğini kontrol eden bir fonksiyon
+const renderAppOrDisabled = () => {
+  const currentDate = dayjs();
+  const disableDate = dayjs("2024-05-01");
+
+  if (currentDate.isAfter(disableDate)) {
+    // Mevcut tarih belirlenen tarihten sonra ise, uygulamayı engelle
+    return <AppDisabled />;
+  } else {
+    // Aksi takdirde, uygulamayı normal şekilde render et
+    return (
+      <React.StrictMode>
+        <ConfigProvider locale={trTR}>
+          <Router>
+            <AppProvider>
+              <RecoilRoot>
+                <App />
+              </RecoilRoot>
+            </AppProvider>
+          </Router>
+        </ConfigProvider>
+      </React.StrictMode>
+    );
+  }
+};
+
+// Uygulamayı render et
+ReactDOM.createRoot(document.getElementById("root")).render(renderAppOrDisabled());
