788982b9b633e41faf5e565190dfed6f3f72441e
Anar Mamedov <anar26271@gmail.com>
2024-04-07 22:47:12 +0300

    Update Table.jsx

--- src/pages/Yonetim/VardiyaTanimlari/Table/Table.jsx
+++ src/pages/Yonetim/VardiyaTanimlari/Table/Table.jsx
@@ -222,8 +222,8 @@ export default function MainTable() {
           dataSource={searchTerm ? filteredData : data}
           pagination={{
             defaultPageSize: 10,
-            showSizeChanger: false,
-            // pageSizeOptions: ["10", "20", "50", "100"],
+            showSizeChanger: true,
+            pageSizeOptions: ["10", "20", "50", "100"],
             position: ["bottomRight"],
           }}
           onRow={onRowClick}
