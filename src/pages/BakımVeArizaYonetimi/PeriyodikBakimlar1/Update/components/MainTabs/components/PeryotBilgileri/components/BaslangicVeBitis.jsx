37f24c0a6467cdb527f260882e88decd10d3ebbc
Anar Mamedov <anar26271@gmail.com>
2024-05-13 22:00:21 +0300

    test

---
+++ src/pages/BakımVeArizaYonetimi/PeriyodikBakimlar1/Update/components/MainTabs/components/PeryotBilgileri/components/BaslangicVeBitis.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BaslangicVeBitis(props) {
+  return <div></div>;
+}
+
+export default BaslangicVeBitis;
---
+++ src/pages/BakımVeArizaYonetimi/PeriyodikBakimlar1/Update/components/MainTabs/components/TanimliMakineler.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TanimliMakineler(props) {
+  return <div>TanimliMakineler</div>;
+}
+
+export default TanimliMakineler;
