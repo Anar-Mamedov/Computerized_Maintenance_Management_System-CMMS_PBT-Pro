09c0a2cdb36e58e3045f3fe9c3603b4232d492b0
Anar Mamedov <anar26271@gmail.com>
2024-05-31 15:13:30 +0300

    personel tablosu arama işlevi atandı

--- src/pages/YardimMasasi/IsTalepleri/components/IsEmrineCevir/Teknisyen/components/PersonelTablo.jsx
+++ src/pages/YardimMasasi/IsTalepleri/components/IsEmrineCevir/Teknisyen/components/PersonelTablo.jsx
@@ -1,6 +1,25 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Button, Modal, Table } from "antd";
+import { Button, Input, Modal, Table } from "antd";
 import AxiosInstance from "../../../../../../../api/http";
+
+// Türkçe karakterleri İngilizce karşılıkları ile değiştiren fonksiyon
+const normalizeText = (text) => {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/ğ/g, "g")
+    .replace(/Ğ/g, "G")
+    .replace(/ü/g, "u")
+    .replace(/Ü/g, "U")
+    .replace(/ş/g, "s")
+    .replace(/Ş/g, "S")
+    .replace(/ı/g, "i")
+    .replace(/İ/g, "I")
+    .replace(/ö/g, "o")
+    .replace(/Ö/g, "O")
+    .replace(/ç/g, "c")
+    .replace(/Ç/g, "C");
+};
 
 export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -8,6 +27,9 @@ export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const [searchTerm1, setSearchTerm1] = useState("");
+  const [filteredData1, setFilteredData1] = useState([]);
 
   const columns = [
     {
@@ -83,7 +105,9 @@ export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
 
   const handleModalOk = () => {
     // Seçilen tüm verileri onSubmit fonksiyonuna gönder
-    const selectedData = data.filter((item) => selectedRowKeys.includes(item.key));
+    const selectedData = data.filter((item) =>
+      selectedRowKeys.includes(item.key)
+    );
     if (selectedData.length > 0) {
       onSubmit && onSubmit(selectedData);
     }
@@ -100,6 +124,27 @@ export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
     setSelectedRowKeys(selectedKeys);
   };
 
+  // Arama işlevselliği için handleSearch fonksiyonları
+  const handleSearch1 = (e) => {
+    const value = e.target.value;
+    setSearchTerm1(value);
+    const normalizedSearchTerm = normalizeText(value);
+    if (value) {
+      const filtered = data.filter((item) =>
+        Object.keys(item).some(
+          (key) =>
+            item[key] &&
+            normalizeText(item[key].toString())
+              .toLowerCase()
+              .includes(normalizedSearchTerm.toLowerCase())
+        )
+      );
+      setFilteredData1(filtered);
+    } else {
+      setFilteredData1(data);
+    }
+  };
+
   return (
     <div>
       <Button
@@ -111,7 +156,8 @@ export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
           alignItems: "center",
           justifyContent: "center",
         }}
-        onClick={handleModalToggle}>
+        onClick={handleModalToggle}
+      >
         {" "}
         +{" "}
       </Button>
@@ -121,7 +167,14 @@ export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
         title="Personel"
         open={isModalVisible}
         onOk={handleModalOk}
-        onCancel={handleModalToggle}>
+        onCancel={handleModalToggle}
+      >
+        <Input
+          placeholder="Arama..."
+          value={searchTerm1}
+          onChange={handleSearch1}
+          style={{ width: "300px", marginBottom: "15px" }}
+        />
         <Table
           rowSelection={{
             type: "checkbox", // Birden fazla seçime izin vermek için 'checkbox' olarak ayarla
@@ -129,7 +182,9 @@ export default function PersonelTablo({ workshopSelectedId, onSubmit }) {
             onChange: onRowSelectChange,
           }}
           columns={columns}
-          dataSource={data}
+          dataSource={
+            filteredData1.length > 0 || searchTerm1 ? filteredData1 : data
+          }
           loading={loading}
           scroll={{
             y: "calc(100vh - 360px)",
