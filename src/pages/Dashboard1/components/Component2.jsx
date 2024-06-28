b310ffbe9f4c8b15e78bbeeb75f7ef095015ab70
Anar Mamedov <anar26271@gmail.com>
2024-06-28 04:43:57 +0300

    GridStack.js ile dashboard yapımı

--- src/pages/Dashboard1/components/Component2.jsx
+++ src/pages/Dashboard1/components/Component2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Component2(props) {
   return (
-    <div style={{ width: "300px", height: "200px", backgroundColor: "blue" }}>
+    <div style={{ width: "100%", height: "100%", backgroundColor: "blue" }}>
       Anar2
     </div>
   );
--- src/pages/Dashboard1/components/Component3.jsx
+++ src/pages/Dashboard1/components/Component3.jsx
@@ -1,55 +1,10 @@
-import React, { useState } from "react";
-import { Resizable } from "react-resizable";
-import "react-resizable/css/styles.css";
+import React from "react";
 
 function Component3(props) {
-  const [size, setSize] = useState({ width: 500, height: 370 });
-
-  const onResize = (e, { size }) => {
-    setSize(size);
-  };
-
-  const handleStyle = {
-    position: "absolute",
-    width: "20px",
-    height: "20px",
-    bottom: 0,
-    right: 0,
-    backgroundColor: "#666",
-    cursor: "se-resize",
-    zIndex: 10,
-  };
-
   return (
-    <Resizable
-      width={size.width}
-      height={size.height}
-      onResize={onResize}
-      minConstraints={[300, 200]}
-      handle={
-        <span
-          className="react-resizable-handle"
-          style={handleStyle}
-          onMouseDown={(e) => {
-            e.stopPropagation();
-          }}
-          onTouchStart={(e) => {
-            e.stopPropagation();
-          }}
-        />
-      }
-    >
-      <div
-        style={{
-          width: `${size.width}px`,
-          height: `${size.height}px`,
-          backgroundColor: "orange",
-          position: "relative",
-        }}
-      >
-        Anar3
-      </div>
-    </Resizable>
+    <div style={{ width: "100%", height: "100%", backgroundColor: "orange" }}>
+      Anar3
+    </div>
   );
 }
 
