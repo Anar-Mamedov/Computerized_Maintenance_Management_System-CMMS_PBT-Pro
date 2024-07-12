04f1b842c0a9d66c2157e37b4a18791eb03366ef
Anar Mamedov <anar26271@gmail.com>
2024-07-12 12:58:45 +0300

    Update IsEmriZamanDagilimi.jsx

--- src/pages/Dashboard1/components/IsEmriZamanDagilimi.jsx
+++ src/pages/Dashboard1/components/IsEmriZamanDagilimi.jsx
@@ -408,7 +408,7 @@ function IsEmriZamanDagilimi(props = {}) {
         }}
       >
         <Text
-          title={`İş Emri Listesi (${
+          title={`İş Emirlerinin Zaman Dağılımı (${
             baslamaTarihi ? formatDate(baslamaTarihi) : ""
           } - ${bitisTarihi ? formatDate(bitisTarihi) : ""})`}
           style={{
@@ -420,7 +420,7 @@ function IsEmriZamanDagilimi(props = {}) {
             maxWidth: "calc(100% - 50px)",
           }}
         >
-          İş Emri Listesi
+          İş Emirlerinin Zaman Dağılımı
           {` (${
             baslamaTarihi && bitisTarihi
               ? `${formatDate(baslamaTarihi)} / ${formatDate(bitisTarihi)}`
@@ -594,7 +594,7 @@ function IsEmriZamanDagilimi(props = {}) {
             }}
           >
             <Text
-              title={`İş Emri Listesi (${
+              title={`İş Emirlerinin Zaman Dağılımı (${
                 baslamaTarihi ? formatDate(baslamaTarihi) : ""
               } - ${bitisTarihi ? formatDate(bitisTarihi) : ""})`}
               style={{
@@ -606,7 +606,7 @@ function IsEmriZamanDagilimi(props = {}) {
                 maxWidth: "calc(100% - 50px)",
               }}
             >
-              İş Emri Listesi
+              İş Emirlerinin Zaman Dağılımı
               {` (${
                 baslamaTarihi && bitisTarihi
                   ? `${formatDate(baslamaTarihi)} / ${formatDate(bitisTarihi)}`
