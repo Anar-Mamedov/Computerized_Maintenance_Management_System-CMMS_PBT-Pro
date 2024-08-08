4c13230046a313eaf35cf8784bddb12af30189b2
Anar Mamedov <anar26271@gmail.com>
2024-08-08 13:35:56 +0300

    Update Headers.jsx

--- src/pages/Headers/Headers.jsx
+++ src/pages/Headers/Headers.jsx
@@ -4,6 +4,7 @@ import Kullanici from "./components/Kullanici";
 import Bildirim from "./components/Bildirim";
 import FirmaLogo from "./components/FirmaLogo.jsx";
 import Hatirlatici from "./components/Hatırlatıcı.jsx";
+import AxiosInstance from "../../api/http.jsx";
 
 export default function Header() {
   useEffect(() => {
@@ -22,21 +23,33 @@ export default function Header() {
 
   useEffect(() => {
     const checkInternetConnection = async () => {
+      const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error("Request timed out")), 2000));
+
       try {
-        const response = await fetch("https://www.google.com", { mode: "no-cors" });
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+        const response = await Promise.race([fetch("https://www.google.com", { mode: "no-cors" }), timeout]);
+        if (response) {
+          Veribaglanti();
+          // message.success("İnternet bağlantısi sağlandı", 3); // 3 saniye sonra kaybolur
         }
       } catch (error) {
         message.error("İnternet bağlantınız kesildi", 3); // 3 saniye sonra kaybolur
       }
     };
 
-    const intervalId = setInterval(checkInternetConnection, 15000); // 1 dakika = 60000 ms
+    const intervalId = setInterval(checkInternetConnection, 15000); // 15 saniye = 15000 ms
 
     // Cleanup function to clear the interval when the component unmounts
     return () => clearInterval(intervalId);
   }, []);
+
+  const Veribaglanti = async () => {
+    const timeout = new Promise((_, reject) => setTimeout(() => reject(new Error("Request timed out")), 2000));
+    try {
+      const response = await Promise.race([AxiosInstance.get("VeritabaniBaglantiKontrol"), timeout]);
+    } catch (error) {
+      message.error("Sunucu bağlantınız kesildi", 3); // 3 saniye sonra kaybolur
+    }
+  };
 
   return (
     <div
