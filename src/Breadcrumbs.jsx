f09c5ecc37625cf3ca20af349898e0f95370e389
Anar Mamedov <anar26271@gmail.com>
2024-08-27 20:26:11 +0300

    Müdahele süresi analiz ekranı

--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -1,46 +1,47 @@
-import { Breadcrumb } from 'antd';
-import { Link, useLocation } from 'react-router-dom';
+import { Breadcrumb } from "antd";
+import { Link, useLocation } from "react-router-dom";
 
 const Breadcrumbs = () => {
   const location = useLocation();
-  const pathSnippets = location.pathname.split('/').filter((i) => i);
+  const pathSnippets = location.pathname.split("/").filter((i) => i);
   const breadcrumbNameMap = {
-    '/': 'Ana Sayfa',
-    '/isEmri1': 'İş Emirleri',
-    '/User': 'Profil Ekranı',
-    '/periyodikBakimlar': 'Periyodik Bakımlar',
-    '/otomatikIsEmirleri': 'Otomatik İş Emirleri',
-    '/raporYonetimi': 'Rapor Yönetimi',
-    '/analizler': 'Analizler',
-    '/planlamaTakvimi': 'Planlama Takvimi',
-    '/makine': 'Makine Tanım',
-    '/ekipmanVeritabani': 'Ekipman Veritabanı',
-    '/sayacGuncelleme': 'Sayaç Güncelleme',
-    '/personelIzinleri': 'Personel İzinleri',
-    '/personelNobetleri': 'Personel Nöbetleri',
-    '/personelCalismaPLani': 'Personel Çalışma Planı',
-    '/isTalebiKullanicilari': 'İş Talebi Kullanıcıları',
-    '/formYonetimi': 'Form Yönetimi',
-    '/kodYonetimi': 'Kod Yönetimi',
-    '/otomatikKodlar': 'Otomatik Kodlar',
-    '/servisOncelikSeviyeleri': 'Servis Öncelik Seviyeleri',
-    '/isEmriTipleri': 'İş Emri Tipleri',
-    '/onaylayicilar': 'Onaylayıcılar',
-    '/projeTanimlari': 'Proje Tanımları',
-    '/lokasyon': 'Lokasyon Tanımları',
-    '/vardiyalar': 'Vardiya Tanımları',
-    '/bakimTanimlari': 'Bakım Tanımları',
-    '/arizaTanimlari': 'Arıza Tanımları',
-    '/atolye': 'Atölye Tanımları',
-    '/personeltanimlari': 'Personel Tanımları',
-    '/isTalepleri': 'İş Talepleri',
-    '/demo': 'Demo',
-    '/userid': 'User ID Control',
+    "/": "Ana Sayfa",
+    "/isEmri1": "İş Emirleri",
+    "/User": "Profil Ekranı",
+    "/periyodikBakimlar": "Periyodik Bakımlar",
+    "/otomatikIsEmirleri": "Otomatik İş Emirleri",
+    "/raporYonetimi": "Rapor Yönetimi",
+    "/analizler": "Analizler",
+    "/planlamaTakvimi": "Planlama Takvimi",
+    "/makine": "Makine Tanım",
+    "/ekipmanVeritabani": "Ekipman Veritabanı",
+    "/sayacGuncelleme": "Sayaç Güncelleme",
+    "/personelIzinleri": "Personel İzinleri",
+    "/personelNobetleri": "Personel Nöbetleri",
+    "/personelCalismaPLani": "Personel Çalışma Planı",
+    "/isTalebiKullanicilari": "İş Talebi Kullanıcıları",
+    "/formYonetimi": "Form Yönetimi",
+    "/kodYonetimi": "Kod Yönetimi",
+    "/otomatikKodlar": "Otomatik Kodlar",
+    "/servisOncelikSeviyeleri": "Servis Öncelik Seviyeleri",
+    "/isEmriTipleri": "İş Emri Tipleri",
+    "/onaylayicilar": "Onaylayıcılar",
+    "/projeTanimlari": "Proje Tanımları",
+    "/lokasyon": "Lokasyon Tanımları",
+    "/vardiyalar": "Vardiya Tanımları",
+    "/bakimTanimlari": "Bakım Tanımları",
+    "/arizaTanimlari": "Arıza Tanımları",
+    "/atolye": "Atölye Tanımları",
+    "/personeltanimlari": "Personel Tanımları",
+    "/isTalepleri": "İş Talepleri",
+    "/demo": "Demo",
+    "/userid": "User ID Control",
+    "/mudaheleSuresi": "Müdahale Süresi",
     // Diğer route'lar için ekleyin...
   };
 
   const breadcrumbItems = pathSnippets.map((_, index) => {
-    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+    const url = `/${pathSnippets.slice(0, index + 1).join("/")}`;
     return {
       key: url,
       title: <Link to={url}>{breadcrumbNameMap[url]}</Link>,
@@ -49,13 +50,13 @@ const Breadcrumbs = () => {
 
   const items = [
     {
-      key: 'home',
-      title: <Link to="/">{breadcrumbNameMap['/']}</Link>,
+      key: "home",
+      title: <Link to="/">{breadcrumbNameMap["/"]}</Link>,
     },
     ...breadcrumbItems,
   ];
 
-  return <Breadcrumb style={{ margin: '7px 0' }} items={items} />;
+  return <Breadcrumb style={{ margin: "7px 0" }} items={items} />;
 };
 
 export default Breadcrumbs;
