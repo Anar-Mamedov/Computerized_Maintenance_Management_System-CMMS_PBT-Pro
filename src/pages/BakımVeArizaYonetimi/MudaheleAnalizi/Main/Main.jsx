c37a2852436b268db59bd88ec677d1cd9d5b1a91
Anar Mamedov <anar26271@gmail.com>
2024-08-27 21:22:56 +0300

    Müdahale Süresi

--- src/pages/BakımVeArizaYonetimi/MudaheleAnalizi/Main/Main.jsx
+++ src/pages/BakımVeArizaYonetimi/MudaheleAnalizi/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainTable from "./../Table/Table.jsx";
 import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import ShortInfo from "../components/Component1.jsx";
 
 function Main(props) {
   const {
@@ -12,7 +13,8 @@ function Main(props) {
   } = useFormContext();
 
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <ShortInfo />
       <MainTable />
     </div>
   );
