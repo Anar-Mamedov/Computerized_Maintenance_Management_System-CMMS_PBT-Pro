190589b9ac45aa188ecc4c9092d2e9d6618af645
Anar Mamedov <anar26271@gmail.com>
2024-08-30 21:27:31 +0300

    test

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,90 +1,85 @@
 import React, { useState, useEffect } from "react";
-import bg from "../../../../assets/images/bg-card.png";
-import { Spin, Typography } from "antd";
-import { ClockCircleOutlined } from "@ant-design/icons";
+import MainTable from "./../Table/Table.jsx";
 import { useFormContext } from "react-hook-form";
-import AxiosInstance from "../../../../api/http.jsx";
+import ShortInfo from "../components/Component1.jsx";
+import Filters from "./../Filters/Filters.jsx";
+import IsTalebiTipleriAtolyeler from "./components/IsTalebiTipleriAtolyeler.jsx";
+import PersonellerMudahaleSuresi from "./components/PersonellerMudahaleSuresi.jsx";
+import MudaheleSuresiHistogram from "../components/MudaheleSuresiHistogram.jsx";
+import AylikOrtalamaMudaheleSuresi from "../components/AylikOrtalamaMudaheleSuresi.jsx";
+import AylikOrtalamaMudaheleSuresiTable from "../components/AylikOrtalamaMudaheleSuresiTable.jsx";
 
-const { Text } = Typography;
+function Main(props) {
+  const {
+    control,
+    watch,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useFormContext();
 
-function Component1() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(null);
-  const { watch } = useFormContext();
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 850);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  const lokasyonId = watch("locationIds");
-  const atolyeId = watch("atolyeIds");
-  const baslangicTarihi = watch("baslangicTarihi");
-  const bitisTarihi = watch("bitisTarihi");
-
-  const fetchData = async () => {
-    setIsLoading(true);
-    const body = {
-      LokasyonId: lokasyonId || "",
-      AtolyeId: atolyeId || "",
-      BaslangicTarih: baslangicTarihi || "",
-      BitisTarih: bitisTarihi || "",
-    };
-    try {
-      const response = await AxiosInstance.post(`GetMudahaleAnalizDashboard`, body);
-      setData(response);
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-    } finally {
-      setIsLoading(false);
-    }
+  // handleScroll fonksiyonunu useEffect dışında tanımlıyoruz
+  const handleScroll = (e) => {
+    console.log("Scroll Y:", e.target.scrollTop);
+    setIsScrolled(e.target.scrollTop > 50);
   };
 
   useEffect(() => {
-    fetchData();
-  }, [lokasyonId, atolyeId, baslangicTarihi, bitisTarihi]);
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > 850);
+    };
 
-  const renderCard = (value, label, backgroundColor) => (
-    <div
-      style={{
-        flex: "1 1 16%", // Değiştirdim
-        maxWidth: "16%", // Değiştirdim
-        background: backgroundColor || `url(${bg}), linear-gradient(rgb(27 17 92), #007eff)`,
-        backgroundPosition: "inherit",
-        backgroundSize: "cover",
-        borderRadius: "5px",
-        padding: "10px",
-        marginBottom: "0px",
-      }}
-    >
-      {isLoading ? (
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-          <Spin size="large" style={{ color: "#fff" }} />
-        </div>
-      ) : (
-        <div style={{ display: "flex", justifyContent: "space-between", cursor: "pointer" }}>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <Text style={{ fontWeight: "500", fontSize: "35px", color: "white" }}>{value !== undefined ? value : ""}</Text>
-            <Text style={{ color: "white", fontSize: "15px", fontWeight: "400" }}>{label}</Text>
-          </div>
-          {/*<ClockCircleOutlined style={{ fontSize: "60px", color: "rgba(255,255,255,.8)" }} />*/}
-        </div>
-      )}
-    </div>
-  );
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: "10px",
-        justifyContent: "space-between",
-      }}
+      style={{ display: "flex", flexDirection: "column", gap: "10px", overflow: "auto", height: "calc(100vh - 170px)" }}
+      onScroll={handleScroll} // handleScroll artık burada tanımlı ve kullanılabilir
     >
-      {renderCard(data?.ToplamTalepSayisi, "Toplam Talep Sayısı", "linear-gradient(to right, #ff7e5f, #feb47b)")}
-      {renderCard(data?.OrtalamaMudahaleSuresi, "Ortalama Müdahale Süresi", "linear-gradient(to right, #6a11cb, #2575fc)")}
-      {renderCard(data?.EnHizliMudahaleSuresi, "En Hızlı Müdahale Süresi", "linear-gradient(to right, #43cea2, #185a9d)")}
-      {renderCard(data?.EnYavasMudahaleSuresi, "En Yavaş Müdahale Süresi", "linear-gradient(to right, #ff4e50, #f9d423)")}
-      {renderCard(data?.OrtalamaCalismaSuresi, "Ortalama Çalışma Süresi", "linear-gradient(to right, #00c6ff, #0072ff)")}
-      {renderCard(data?.ToplamCalismaSuresi, "Toplam Çalışma Süresi", "linear-gradient(to right, #f7971e, #ffd200)")}
+      <div
+        style={{
+          position: "sticky",
+          top: 7,
+          zIndex: 4,
+          margin: "7px 7px 0px 7px",
+          padding: "7px",
+          backgroundColor: "white",
+          border: "1px solid rgb(240, 240, 240)",
+          borderRadius: "5px",
+          filter: isScrolled ? "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.2))" : "none",
+          transition: "filter 0.3s ease",
+        }}
+      >
+        <Filters />
+      </div>
+      <ShortInfo />
+      <div style={{ display: "flex", gap: "10px" }}>
+        <MudaheleSuresiHistogram />
+        <AylikOrtalamaMudaheleSuresi />
+      </div>
+      <AylikOrtalamaMudaheleSuresiTable />
+
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          flexDirection: isWideScreen ? "row" : "column",
+        }}
+      >
+        <IsTalebiTipleriAtolyeler />
+        <PersonellerMudahaleSuresi />
+      </div>
+      <MainTable />
     </div>
   );
 }
 
-export default Component1;
+export default Main;
