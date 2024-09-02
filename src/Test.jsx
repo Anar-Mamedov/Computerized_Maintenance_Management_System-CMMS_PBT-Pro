a20fe6050356b83a43663e4e10402d30a627d828
Anar Mamedov <anar26271@gmail.com>
2024-09-02 14:32:09 +0300

    test

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,402 +1,111 @@
-import { useEffect, useState } from "react";
-import { Route, Routes, Link, useLocation, Outlet, Navigate } from "react-router-dom";
-import { Breadcrumb, Layout, Menu, theme, Button, Typography, Input } from "antd";
-import { DesktopOutlined, FileOutlined, PieChartOutlined, MenuUnfoldOutlined, MenuFoldOutlined, TeamOutlined, UserOutlined, KeyOutlined } from "@ant-design/icons";
-// import Isemri from "./pages/DashboardAnalytics2/Isemri";
-import IsEmri from "./pages/BakımVeArizaYonetimi/IsEmri/IsEmri";
-import MakineTanim from "./pages/MakineEkipman/MakineTanim/MakineTanim";
-import LokasyonTanim from "./pages/Yonetim/LokasyonTanimlari/LokasyonTanimlari";
-import VardiyaTanim from "./pages/Yonetim/VardiyaTanimlari/VardiyaTanimlari";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import BakimTanimlari from "./pages/BakımVeArizaYonetimi/BakimTanimlari/BakimTanimlari";
-import ArizaTanimlari from "./pages/BakımVeArizaYonetimi/ArizaTanimlari/ArizaTanimlari";
-import AtolyeTanimlari from "./pages/PersonelYonetimi/AtolyeTanimlari/AtolyeTanimlari";
-import PersonelTanimlari from "./pages/PersonelYonetimi/PersonelTanimlari/PersonelTanimlari";
-import IsTalepleri from "./pages/YardimMasasi/IsTalepleri/IsTalepleri";
-import Hazirlaniyor from "./pages/Hazirlaniyor";
-import Auth from "./pages/Auth/Auth";
-import logo from "../src/assets/images/logoBeyaz.png";
-import Headers from "./pages/Headers/Headers";
-import { useRecoilState } from "recoil";
-import { userState } from "./state/userState";
-import PlanlamaTakvimi from "./pages/BakımVeArizaYonetimi/PlanlamaTakvimi/PlanlamaTakvimi";
-import OtomatikIsEmri from "./pages/BakımVeArizaYonetimi/OtomatikIsEmri/OtomatikIsEmri";
-import PeriyodikBakimlar1 from "./pages/BakımVeArizaYonetimi/PeriyodikBakimlar1/PeryodikBakimlar";
-import RaporYonetimi from "./pages/Rapor&Formlar/RaporYonetimi/RaporYonetimi.jsx";
-import ProfilEkrani from "./pages/Headers/components/ProfilEkrani/ProfilEkrani.jsx";
-import Analizler from "./pages/PersonelYonetimi/PersonelKPI/Analizler.jsx";
-import Dashboard1 from "./pages/Dashboard1/Dashboard.jsx";
-import UserIdControl from "./pages/UserIdControl/UserIdControl.jsx";
-import IsEmriTipleri from "./pages/Yonetim/IsEmriTipleri/IsEmriTipleri.jsx";
-import Breadcrumbs from "./Breadcrumbs"; // Import the Breadcrumbs component
-import MudaheleSuresi from "./pages/BakımVeArizaYonetimi/MudaheleAnalizi/MudaheleAnalizi.jsx";
-import SearchComponent from "./pages/Headers/components/SearchComponent.jsx";
+import React, { useEffect, useState } from "react";
+import { Button, Modal, message } from "antd";
+import { CheckOutlined } from "@ant-design/icons";
+import { Controller, useForm, FormProvider } from "react-hook-form";
+import AxiosInstance from "../../../../../../api/http";
+import dayjs from "dayjs";
+import AtolyeIsEmriCevir from "./AtolyeIsEmriCevir";
 
-const { Text } = Typography;
-const { TextArea } = Input;
-
-const { Header, Content, Footer, Sider } = Layout;
-
-const loginData = JSON.parse(localStorage.getItem("login")) || {};
-
-function getItem(label, key, icon, children, isClickable = true) {
-  return {
-    key,
-    icon,
-    children,
-    label: isClickable ? <Link to={`/${key}`}>{label}</Link> : label,
-  };
-}
-
-function filterItems(items) {
-  // LocalStorage'dan token kontrolü
-  const token = localStorage.getItem("token");
-
-  // Token yoksa, hiçbir filtreleme yapmadan tüm öğeleri döndür
-  if (!token) {
-    return items;
-  }
-
-  // Token varsa, filtreleme işlemini gerçekleştir
-  return items
-    .map((item) => {
-      // "Ana Sayfa" için özel durumu kontrol et
-      if (item.key === "" && item.label.props.children === "Ana Sayfa") {
-        return item; // "Ana Sayfa" her zaman görünür
-      }
-
-      const filteredChildren = item.children ? filterItems(item.children).filter((child) => loginData[child.key]) : [];
-      return {
-        ...item,
-        children: filteredChildren.length > 0 ? filteredChildren : undefined,
-      };
-    })
-    .filter((item) => item.children || loginData[item.key] || (item.key === "" && item.label.props.children === "Ana Sayfa"));
-}
-
-const rawItems = [
-  getItem("Ana Sayfa", "", <PieChartOutlined />),
-  // getItem("Option 1", "option1", <PieChartOutlined />),
-  // getItem("Option 2", "option2", <DesktopOutlined />),
-  getItem(
-    "Makine & Ekipman Yönetimi",
-    "makine&ekipman",
-    <TeamOutlined />,
-    [
-      getItem("Makine Tanım", "makine", true),
-      getItem("Ekipman Veritabanı", "ekipmanVeritabani", true),
-      getItem("Sayaç Güncelleme", "sayacGuncelleme", true),
-      // getItem("Team 2", "team2", true)
-    ],
-    false
-  ),
-  getItem(
-    "Bakım ve Arıza Yönetimi",
-    "bakim&ariza",
-    <UserOutlined />,
-    [
-      // getItem("Tom", "tom", true),
-      getItem("Bakım Tanımları", "bakimTanimlari", true),
-      getItem("Arıza Tanımları", "arizaTanimlari", true),
-      // getItem("İş Emri", "isemri", true),
-      getItem("İş Emirleri", "isEmri1", true),
-      getItem("Periyodik Bakımlar", "periyodikBakimlar", true),
-      getItem("Otomatik İş Emirleri", "otomatikIsEmirleri", true),
-      getItem("Planlama Takvimi", "planlamaTakvimi", true),
-      getItem("Müdahele Süresi", "mudaheleSuresi", true),
-      // getItem("Alex", "alex", true)
-    ],
-    false
-  ),
-  getItem(
-    "Personel Yönetimi",
-    "personelYonetimi",
-    <UserOutlined />,
-    [
-      getItem("Atölye Tanımları", "atolye", true),
-      getItem("Personel Tanımları", "personeltanimlari", true),
-      getItem("Personel İzinleri", "personelIzinleri", true),
-      getItem("Personel Nöbetleri", "personelNobetleri", true),
-      getItem("Personel Çalışma Planı", "personelCalismaPLani", true),
-      getItem("Personel KPI", "analizler", true),
-
-      // getItem("Team 2", "team2", true)
-    ],
-    false
-  ),
-  getItem(
-    "Yardım Masası",
-    "yardimMasasi",
-    <KeyOutlined />,
-    [
-      getItem("İş Talepleri", "isTalepleri", true),
-      getItem("İş Talebi Kullanıcıları", "isTalebiKullanicilari", true),
-      // getItem("Team 2", "team2", true)
-    ],
-    false
-  ),
-  getItem(
-    "Rapor & Formlar",
-    "rapor&formlar",
-    <KeyOutlined />,
-    [
-      getItem("Rapor Yönetimi", "raporYonetimi", true),
-      getItem("Form Yönetimi", "formYonetimi", true),
-      // getItem("Team 2", "team2", true)
-    ],
-    false
-  ),
-  getItem(
-    "Yönetim",
-    "yonetim",
-    <KeyOutlined />,
-    [
-      getItem("Lokasyon Tanımları", "lokasyon", true),
-      getItem("Vardiya Tanımları", "vardiyalar", true),
-      getItem("Kod Yönetimi", "kodYonetimi", true),
-      getItem("Otomatik Kodlar", "otomatikKodlar", true),
-      getItem("Servis Öncelik Seviyeleri", "servisOncelikSeviyeleri", true),
-      getItem("İş Emri Tipleri", "isEmriTipleri", true),
-      getItem("Onaylayıcılar", "onaylayicilar", true),
-      getItem("Proje Tanımları", "projeTanimlari", true),
-      // getItem("Team 2", "team2", true)
-    ],
-    false
-  ),
-  // getItem("Files", "files", <FileOutlined />),
-];
-
-// const Option1Page = () => <div>Option 1 Content</div>;
-// const BillPage = () => <div>Bill is a cat.</div>;
-// Diğer sayfa bileşenlerinizi burada tanımlayın...
-
-// diğer menüyü açtığımda diğer menüyü kapatıyor ve sayfa yüklendiğinde açık olan menüyü açık bırakıyor
-// diğer menüyü açtığımda diğer menüyü kapatıyor ve sayfa yüklendiğinde açık olan menüyü açık bırakıyor sonu
-
-export default function App() {
-  const [user, setUser] = useRecoilState(userState);
-
-  // localStorage'dan kullanıcı bilgilerini oku
-  useEffect(() => {
-    // Sayfa yüklendiğinde, kullanıcı bilgilerini localStorage'dan oku
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser)); // Recoil durumunu güncelle
-    }
-  }, [setUser]);
-  // localStorage'dan kullanıcı bilgilerini oku son
-
-  return (
-    <Routes>
-      <Route path="/auth" element={<Auth />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <BaseLayout />
-          </ProtectedRoute>
-        }
-      >
-        {loginData?.Dashboard && <Route path="/" element={<Dashboard1 />} />}
-        {/* <Route path="/isemri" element={<Isemri />} /> */}
-        <Route path="/isEmri1" element={<IsEmri />} />
-        <Route path="/User" element={<ProfilEkrani />} />
-        <Route path="/periyodikBakimlar" element={<PeriyodikBakimlar1 />} />
-        <Route path="/otomatikIsEmirleri" element={<OtomatikIsEmri />} />
-        <Route path="/raporYonetimi" element={<RaporYonetimi />} />
-        <Route path="/analizler" element={<Analizler />} />
-        <Route path="/planlamaTakvimi" element={<PlanlamaTakvimi />} />
-        <Route path="/makine" element={<MakineTanim />} />
-        <Route path="/ekipmanVeritabani" element={<Hazirlaniyor />} />
-        <Route path="/sayacGuncelleme" element={<Hazirlaniyor />} />
-        <Route path="/personelIzinleri" element={<Hazirlaniyor />} />
-        <Route path="/personelNobetleri" element={<Hazirlaniyor />} />
-        <Route path="/personelCalismaPLani" element={<Hazirlaniyor />} />
-        <Route path="/isTalebiKullanicilari" element={<Hazirlaniyor />} />
-        <Route path="/raporYonetimi" element={<Hazirlaniyor />} />
-        <Route path="/formYonetimi" element={<Hazirlaniyor />} />
-        <Route path="/kodYonetimi" element={<Hazirlaniyor />} />
-        <Route path="/otomatikKodlar" element={<Hazirlaniyor />} />
-        <Route path="/servisOncelikSeviyeleri" element={<Hazirlaniyor />} />
-        <Route path="/isEmriTipleri" element={<IsEmriTipleri />} />
-        <Route path="/onaylayicilar" element={<Hazirlaniyor />} />
-        <Route path="/projeTanimlari" element={<Hazirlaniyor />} />
-        <Route path="/lokasyon" element={<LokasyonTanim />} />
-        <Route path="/vardiyalar" element={<VardiyaTanim />} />
-        <Route path="/bakimTanimlari" element={<BakimTanimlari />} />
-        <Route path="/arizaTanimlari" element={<ArizaTanimlari />} />
-        <Route path="/atolye" element={<AtolyeTanimlari />} />
-        <Route path="/personeltanimlari" element={<PersonelTanimlari />} />
-        <Route path="/isTalepleri" element={<IsTalepleri />} />
-        <Route path="/demo" element={<Dashboard />} />
-        <Route path="/userid" element={<UserIdControl />} />
-        <Route path="/mudaheleSuresi" element={<MudaheleSuresi />} />
-      </Route>
-    </Routes>
-  );
-}
-
-const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
-  // const token = sessionStorage.getItem("token");
-
-  if (!token) {
-    return <Navigate to={"/auth"} replace />;
-  }
-
-  return children;
-};
-
-const BaseLayout = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const [selectedKey, setSelectedKey] = useState(""); // Seçilen menü öğesi için state
-  const [mobileView, setMobileView] = useState(window.innerWidth < 768);
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
-  useEffect(() => {
-    // Uygulama yüklendiğinde mevcut path'i setSelectedKey ile belirleyin
-    setSelectedKey(location.pathname);
-  }, [location]);
-
-  const toggleCollapsed = () => {
-    setCollapsed(!collapsed);
-  };
-
-  // Ekran boyutu değişikliklerini dinle
-  window.addEventListener("resize", () => {
-    setMobileView(window.innerWidth < 768);
+export default function AtolyeSubmit({ selectedRows, refreshTableData }) {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [loading, setLoading] = useState(false); // Add loading state
+  const methods = useForm({
+    defaultValues: {
+      atolyeTanim: "",
+      atolyeID: "",
+    },
   });
+  const { setValue, reset, handleSubmit, watch } = methods;
 
-  const logoStyle = {
-    marginTop: "20px", // Yukarıda 20 piksellik boşluk bırakın
-    maxWidth: "100px", // Genişliği sabit tutun
-    width: "80%", // Genişliği sabit tutun
-    // marginBottom: "20px", // Aşağıda 20 piksellik boşluk bırakın
+  const formatDateWithDayjs = (dateString) => {
+    const formattedDate = dayjs(dateString);
+    return formattedDate.isValid() ? formattedDate.format("YYYY-MM-DD") : "";
+  };
+
+  const formatTimeWithDayjs = (timeObj) => {
+    const formattedTime = dayjs(timeObj);
+    return formattedTime.isValid() ? formattedTime.format("HH:mm:ss") : "";
+  };
+
+  const atolyeID = watch("atolyeID");
+
+  useEffect(() => {
+    const tbIsTalepId = selectedRows.map((row) => row.key).join(",");
+    const isValidStatus = selectedRows.every((row) => [0, 1, 2].includes(row.IST_DURUM_ID));
+
+    setIsButtonDisabled(!atolyeID || !tbIsTalepId || !isValidStatus);
+  }, [atolyeID, selectedRows]);
+
+  const onSubmited = (data) => {
+    setLoading(true); // Set loading to true when form is submitted
+    const atolyeIDValue = watch("atolyeID");
+
+    const Body = selectedRows.map((row) => ({
+      TALEP_ID: row.key,
+      ATOLYE_ID: atolyeIDValue,
+      ISM_WEB: true,
+      TEKNISYEN_IDS: [],
+    }));
+
+    AxiosInstance.post(`IsTalepToIsEmri`, Body)
+      .then((response) => {
+        console.log("Data sent successfully:", response);
+        reset();
+        setTimeout(() => {
+          refreshTableData();
+        }, 1000);
+        if (response.status_code === 200 || response.status_code === 201) {
+          const aciklamaValues = response.isEmriNolari.map((item) => item.Aciklama).join(", ");
+          message.success(aciklamaValues + " Numaralı İş Emirleri Oluşturulmuştur.");
+        } else if (response.status_code === 401) {
+          message.error("Bu işlemi yapmaya yetkiniz bulunmamaktadır.");
+        } else {
+          message.error("Ekleme Başarısız.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error sending data:", error);
+        if (navigator.onLine) {
+          message.error("Hata Mesajı: " + error.message);
+        } else {
+          message.error("Internet Bağlantısı Mevcut Değil.");
+        }
+      })
+      .finally(() => {
+        setLoading(false); // Reset loading state after form submission is complete
+      });
+
+    console.log({ Body });
   };
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      {(mobileView && (
-        <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed} breakpoint="lg" collapsedWidth="0.0000000000001">
-          <div
-            className="demo-logo-vertical"
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-              alignItems: "flex-end",
-              marginBottom: "20px",
-              columnGap: "5px",
-            }}
-          >
-            <img src={logo} alt="Logo" style={logoStyle} />
-            <Text style={{ color: "white", marginTop: "11px" }}>v. 1.7.0</Text>
-          </div>
-          <MenuWrapper />
-        </Sider>
-      )) || (
-        <Sider
-          collapsible
-          collapsed={collapsed}
-          onCollapse={setCollapsed}
-          width={250} // Genişletilmiş durumda Sider'ın genişliği
-          collapsedWidth={70} // Daraltılmış durumda Sider'ın genişliği
-          breakpoint="lg" // breakpoint="lg" olduğunda collapsedWidth değerini kullanır
-        >
-          <div
-            className="demo-logo-vertical"
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-              alignItems: "flex-end",
-              marginBottom: "20px",
-              columnGap: "5px",
-            }}
-          >
-            <img src={logo} alt="Logo" style={logoStyle} />
-            <Text style={{ color: "white", marginTop: "11px" }}>v. 1.7.0</Text>
-          </div>
-          <MenuWrapper />
-        </Sider>
-      )}
-
-      <Layout>
-        <Header
+    <FormProvider {...methods}>
+      <div style={{ display: "flex", width: "100%", maxWidth: "315px" }}>
+        <form style={{ width: "100%" }} onSubmit={methods.handleSubmit(onSubmited)}>
+          <AtolyeIsEmriCevir selectedRows={selectedRows} />
+        </form>
+        <Button
           style={{
-            padding: 0,
-            background: colorBgContainer,
+            padding: "0px 0px",
+            width: "32px",
+            height: "32px",
             display: "flex",
             alignItems: "center",
+            justifyContent: "center",
           }}
+          type="submit"
+          onClick={methods.handleSubmit(onSubmited)}
+          disabled={isButtonDisabled || loading} // Disable button if loading
+          loading={loading} // Show loading spinner
         >
-          {mobileView && (
-            <Button
-              onClick={toggleCollapsed}
-              style={{
-                padding: "0 24px",
-                width: "40px",
-                height: "40px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginLeft: "10px",
-              }}
-            >
-              {collapsed ? <MenuUnfoldOutlined style={{ color: "#0066ff" }} /> : <MenuFoldOutlined style={{ color: "#0066ff" }} />}
-            </Button>
-          )}
-          <Headers />
-        </Header>
-        <Content style={{ margin: mobileView ? "0 0px" : "0 16px" }}>
-          <Breadcrumbs />
-          <div
+          <CheckOutlined
             style={{
-              padding: mobileView ? "24px 0px" : 24,
-              borderRadius: "16px",
-              minHeight: 360,
-              height: "calc(100vh - 132px)",
-              background: colorBgContainer,
+              color: isButtonDisabled ? "grey" : "rgb(0, 211, 0)",
+              fontSize: "18px",
             }}
-          >
-            <Outlet />
-          </div>
-        </Content>
-
-        <Footer style={{ textAlign: "center", padding: "10px 50px" }}>Orjin {new Date().getFullYear()} - Design & Develop by Orjin Team</Footer>
-      </Layout>
-    </Layout>
+          />
+        </Button>
+      </div>
+    </FormProvider>
   );
-};
-
-const items = filterItems(rawItems);
-
-const MenuWrapper = () => {
-  const location = useLocation();
-  const [openKeys, setOpenKeys] = useState([]);
-
-  useEffect(() => {
-    const currentPath = location.pathname.split("/")[1];
-    const openKey = items.find((item) => item.children?.some((child) => `/${child.key}` === `/${currentPath}`))?.key;
-    if (openKey) {
-      setOpenKeys([openKey]);
-    }
-  }, [location]);
-
-  const onOpenChange = (keys) => {
-    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
-    if (items.map((item) => item.key).indexOf(latestOpenKey) === -1) {
-      setOpenKeys(keys);
-    } else {
-      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-    }
-  };
-
-  return (
-    <div style={{ height: "calc(100vh - 115px)", overflow: "auto" }}>
-      <Menu theme="dark" defaultSelectedKeys={[location.pathname.split("/")[1]]} mode="inline" items={items} openKeys={openKeys} onOpenChange={onOpenChange} />
-    </div>
-  );
-};
+}
