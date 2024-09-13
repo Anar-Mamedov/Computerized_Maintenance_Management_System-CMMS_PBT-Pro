6c10675a51aff21d0ad5ec7880100928e610a8b6
Anar Mamedov <anar26271@gmail.com>
2024-09-13 17:33:53 +0300

    logout button

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,177 +1,82 @@
-import React, { useState, useEffect } from "react";
-import { Button, Input, Popover, message, Typography } from "antd";
-import { CameraOutlined, CloseOutlined } from "@ant-design/icons";
-import { FaQuestion } from "react-icons/fa";
-import html2canvas from "html2canvas";
-import axios from "axios";
-import styled from "styled-components";
+import React, { useState, useEffect, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { ConfigProvider } from "antd";
+import trTR from "antd/es/locale/tr_TR";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppProvider } from "./AppContext.jsx";
+import { RecoilRoot } from "recoil";
 import dayjs from "dayjs";
+import AxiosInstance from "./api/http.jsx";
+import "./index.css";
 
-const { Text } = Typography;
+// App bileşenini lazy load ile yükleyin
+const App = React.lazy(() => import("./App.jsx"));
 
-const ErrorText = styled(Text)`
-  color: red;
-  margin-top: 5px;
-`;
+// Uygulamanın devre dışı olduğu durumu gösteren bileşen
+const AppDisabledComponent = () => <div style={{ textAlign: "center", marginTop: "20%" }}></div>;
+const AppDisabled = React.memo(AppDisabledComponent);
 
-const FloatButton = () => {
-  const [imageData, setImageData] = useState(null);
-  const [inputValue, setInputValue] = useState("");
-  const [textValue, setTextValue] = useState("");
-  const [visible, setVisible] = useState(false);
-  const [baseURL, setBaseURL] = useState("");
-  const [userInfo, setUserInfo] = useState(null);
-  const [inputError, setInputError] = useState(false);
-  const [textError, setTextError] = useState(false);
-
-  const handleScreenshot = () => {
-    html2canvas(document.body, {
-      scale: 2,
-      useCORS: true,
-      logging: true,
-      width: window.innerWidth,
-      height: window.innerHeight,
-      windowWidth: document.documentElement.scrollWidth,
-      windowHeight: document.documentElement.scrollHeight,
-    }).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png", 1.0);
-      setImageData(imgData);
-      setVisible(true);
-    });
-  };
-
-  useEffect(() => {
-    const storedValue = localStorage.getItem("baseURL");
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (storedValue && user) {
-      setBaseURL(storedValue);
-      setUserInfo(user);
-    }
-  }, []);
-
-  const handleSend = async () => {
-    if (!inputValue.trim()) {
-      setInputError(true);
-    } else {
-      setInputError(false);
-    }
-
-    if (!textValue.trim()) {
-      setTextError(true);
-    } else {
-      setTextError(false);
-    }
-
-    if (!inputValue.trim() || !textValue.trim()) {
-      return;
-    }
-
-    try {
-      const formData = new FormData();
-      formData.append("musteribaglantianahtar", baseURL);
-      formData.append("kullaniciId", userInfo.userId);
-      formData.append("kullaniciAdi", userInfo.userName);
-      formData.append("musteridestekbaslik", inputValue);
-      formData.append("musteridestekaciklama", textValue);
-
-      const response = await fetch(imageData);
-      const blob = await response.blob();
-      const timestamp = dayjs().format("YYYYMMDD_HHmmss");
-      formData.append("resim", blob, `screenshot_${timestamp}.png`);
-
-      await axios.post(`http://95.130.173.226:1212/api/HelpDesk/addMusteriDestek`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-
-      message.success("Veriler başarıyla gönderildi!");
-      setVisible(false);
-    } catch (error) {
-      message.error("Verileri gönderirken bir hata oluştu.");
-    }
-  };
-
-  const handleClose = () => {
-    setVisible(false);
-  };
-
-  const handleVisibleChange = (vis) => {
-    if (vis) {
-      handleScreenshot();
-      setInputValue("");
-      setTextValue("");
-      setInputError(false);
-      setTextError(false);
-      setTimeout(() => {
-        const popoverElements = document.getElementsByClassName("ant-popover");
-        if (popoverElements.length > 0) {
-          const popover = popoverElements[0];
-          popover.style.zIndex = "100000";
-          popover.style.position = "fixed";
-          popover.style.bottom = "70px";
-          popover.style.right = "0";
-        }
-      }, 100);
-    }
-  };
-
-  const popoverContent = (
-    <div style={{ maxWidth: "300px", position: "relative" }}>
-      <Button
-        type="primary"
-        danger
-        icon={<CloseOutlined />}
-        style={{ position: "absolute", top: "-30px", right: "0px", width: "20px", height: "20px", padding: "10px" }}
-        onClick={handleClose}
-      />
-      <img src={imageData} alt="Screenshot" style={{ width: "100%", marginBottom: "10px" }} />
-      <div style={{ display: "flex", flexDirection: "column", marginBottom: "5px" }}>
-        <Text>Talep Nedeni:</Text>
-        <Input
-          placeholder="Başlık"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          style={{ marginBottom: "5px", borderColor: inputError ? "red" : undefined }}
-        />
-        {inputError && <ErrorText>Başlık alanı zorunludur.</ErrorText>}
-      </div>
-      <div style={{ display: "flex", flexDirection: "column", marginBottom: "5px" }}>
-        <Text>Açıklama:</Text>
-        <Input.TextArea
-          placeholder="Açıklama"
-          value={textValue}
-          onChange={(e) => setTextValue(e.target.value)}
-          rows={4}
-          style={{ marginBottom: "5px", borderColor: textError ? "red" : undefined }}
-        />
-        {textError && <ErrorText>Açıklama alanı zorunludur.</ErrorText>}
-      </div>
-
-      <Button type="primary" onClick={handleSend} block>
-        Gönder
-      </Button>
-    </div>
-  );
-
-  return (
-    <Popover content={popoverContent} title="Talep Detayları" trigger="click" open={visible} onOpenChange={handleVisibleChange} destroyTooltipOnHide={true}>
-      <Button
-        type="primary"
-        shape="circle"
-        icon={<FaQuestion />}
-        size="large"
-        style={{
-          position: "fixed",
-          bottom: "20px",
-          right: "20px",
-          zIndex: 9999,
-          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-        }}
-        onClick={() => setVisible(true)}
-      />
-    </Popover>
-  );
+// Ana bileşen
+const MainComponent = () => {
+  const baseURL = localStorage.getItem("baseURL");
+  return baseURL ? <MainComponent1 /> : <Main />;
 };
 
-export default FloatButton;
+// Ana uygulama bileşeni
+const MainComponent1 = React.memo(function MainComponent1() {
+  const [disableDate, setDisableDate] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDisableDate = async () => {
+      try {
+        const response = await AxiosInstance.get("GetEndDate");
+        const data = response.data;
+        if (data && data.length > 0) {
+          setDisableDate(dayjs(data[0].ISL_DONEM_BITIS).format("YYYY-MM-DD"));
+        } else {
+          console.error("API responsunda beklenen veri bulunamadı.");
+        }
+      } catch (error) {
+        console.error("API çağrısında hata oluştu:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDisableDate();
+  }, []);
+
+  if (loading) {
+    return <div style={{ textAlign: "center", marginTop: "20%", fontSize: "18px" }}>Yükleniyor...</div>;
+  }
+
+  const currentDate = dayjs();
+
+  if (disableDate && currentDate.isAfter(disableDate)) {
+    return <AppDisabled />;
+  }
+
+  return <Main />;
+});
+
+// Main bileşeni
+const Main = React.memo(function Main() {
+  return (
+    <React.StrictMode>
+      <ConfigProvider locale={trTR}>
+        <Router>
+          <AppProvider>
+            <RecoilRoot>
+              <Suspense fallback={<div>Yükleniyor...</div>}>
+                <App />
+              </Suspense>
+            </RecoilRoot>
+          </AppProvider>
+        </Router>
+      </ConfigProvider>
+    </React.StrictMode>
+  );
+});
+
+// Uygulamayı render et
+ReactDOM.createRoot(document.getElementById("root")).render(<MainComponent />);
