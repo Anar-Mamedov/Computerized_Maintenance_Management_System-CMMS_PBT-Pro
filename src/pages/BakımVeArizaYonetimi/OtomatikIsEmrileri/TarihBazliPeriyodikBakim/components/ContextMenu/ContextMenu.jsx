b1214e4e1a6221e615428fda2da3a5bfc0599dc6
Anar Mamedov <anar26271@gmail.com>
2024-11-14 00:42:09 +0400

    otomatik iş emirleri tamam

--- src/pages/BakımVeArizaYonetimi/OtomatikIsEmrileri/TarihBazliPeriyodikBakim/components/ContextMenu/ContextMenu.jsx
+++ src/pages/BakımVeArizaYonetimi/OtomatikIsEmrileri/TarihBazliPeriyodikBakim/components/ContextMenu/ContextMenu.jsx
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import { Button, Popover, Typography } from "antd";
 import { MoreOutlined } from "@ant-design/icons";
 import Sil from "./components/Sil";
-import Iptal from "./components/Iptal/Iptal";
-import Kapat from "./components/Kapat/Kapat";
-import Parametreler from "./components/Parametreler/Parametreler";
-import TarihceTablo from "./components/TarihceTablo";
-import Ac from "./components/Ac";
-import OnayaGonder from "./components/OnayaGonder.jsx";
+import IsEmriOlustur from "./components/IsEmriOlustur.jsx";
 
 const { Text, Link } = Typography;
 
@@ -21,35 +16,14 @@ export default function ContextMenu({ selectedRows, refreshTableData, onayCheck 
   const hidePopover = () => {
     setVisible(false);
   };
-  // Silme işlemi için disable durumunu kontrol et
-  const isDisabled = selectedRows.some((row) => row.IST_DURUM_ID === 3 || row.IST_DURUM_ID === 4);
-  const iptalDisabled = selectedRows.some((row) => row.IST_DURUM_ID === 3 || row.IST_DURUM_ID === 2 || row.IST_DURUM_ID === 4 || row.IST_DURUM_ID === 5);
-  const kapatDisabled = selectedRows.some((row) => row.KAPALI === true);
 
   const content = (
     <div>
-      {selectedRows.length === 1 && selectedRows.every((row) => row.KAPALI === true) && (
-        <Ac selectedRows={selectedRows} refreshTableData={refreshTableData} disabled={isDisabled} hidePopover={hidePopover} />
-      )}
+      {selectedRows.length >= 1 && <IsEmriOlustur selectedRows={selectedRows} refreshTableData={refreshTableData} hidePopover={hidePopover} />}
 
-      {selectedRows.length >= 1 && selectedRows.every((row) => row.KAPALI === false) && (
-        <Sil selectedRows={selectedRows} refreshTableData={refreshTableData} disabled={isDisabled} hidePopover={hidePopover} />
-      )}
-      {onayCheck &&
-        selectedRows.length >= 1 &&
-        selectedRows.some((row) => row.ISM_ONAY_DURUM === 0 || row.ISM_ONAY_DURUM === 3) &&
-        selectedRows.every((row) => row.KAPALI === false) && <OnayaGonder selectedRows={selectedRows} refreshTableData={refreshTableData} hidePopover={hidePopover} />}
-      {/* <Iptal selectedRows={selectedRows} refreshTableData={refreshTableData} iptalDisabled={iptalDisabled} /> */}
-      {/*{selectedRows.length >= 1 && <Kapat selectedRows={selectedRows} refreshTableData={refreshTableData} kapatDisabled={kapatDisabled} />}*/}
-      {selectedRows.length >= 1 &&
-        (onayCheck ? (
-          selectedRows.every((row) => row.ISM_ONAY_DURUM === 2) && <Kapat selectedRows={selectedRows} refreshTableData={refreshTableData} kapatDisabled={kapatDisabled} />
-        ) : (
-          <Kapat selectedRows={selectedRows} refreshTableData={refreshTableData} kapatDisabled={kapatDisabled} />
-        ))}
-
-      {/* <Parametreler />
-      {selectedRows.length === 1 && <TarihceTablo selectedRows={selectedRows} />} */}
+      {/* {selectedRows.length >= 1 && selectedRows.every((row) => row.KAPALI === false) && (
+        <Sil selectedRows={selectedRows} refreshTableData={refreshTableData} hidePopover={hidePopover} />
+      )} */}
     </div>
   );
   return (
