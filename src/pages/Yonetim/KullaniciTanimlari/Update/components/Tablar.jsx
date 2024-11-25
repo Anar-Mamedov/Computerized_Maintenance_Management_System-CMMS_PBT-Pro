b7d38189ce40dbe2ff76e8bb161a8f1f5fff883a
Anar Mamedov <anar26271@gmail.com>
2024-11-26 01:09:42 +0400

    test

--- src/pages/Yonetim/KullaniciTanimlari/Update/components/Tablar.jsx
+++ src/pages/Yonetim/KullaniciTanimlari/Update/components/Tablar.jsx
@@ -3,6 +3,8 @@ import { Radio, Divider } from "antd";
 import MainTabs from "./MainTabs/MainTabs";
 import Yetkiler from "./Yetkiler/Yetkiler";
 import LokasyonYetkileri from "./LokasyonYetkileri/LokasyonYetkileri1";
+import DepoYetkisi from "./DepoYetkisi/DepoYetkisi";
+import AtolyeYetkisi from "./AtolyeYetkisi/AtolyeYetkisi";
 import { t } from "i18next";
 
 function Tablar() {
@@ -17,12 +19,16 @@ function Tablar() {
       <Radio.Group value={tabKey} onChange={handleTabChange} style={{ display: "flex", justifyContent: "center" }}>
         <Radio.Button value="1">{t("hesap")}</Radio.Button>
         <Radio.Button value="2">{t("lokasyonlar")}</Radio.Button>
-        <Radio.Button value="3">{t("yetkiler")}</Radio.Button>
+        <Radio.Button value="3">{t("depoYetkileri")}</Radio.Button>
+        <Radio.Button value="4">{t("atolyeYetkileri")}</Radio.Button>
+        <Radio.Button value="5">{t("yetkiler")}</Radio.Button>
       </Radio.Group>
       <Divider style={{ marginBottom: 10 }} />
       {tabKey === "1" && <MainTabs />}
       {tabKey === "2" && <LokasyonYetkileri />}
-      {tabKey === "3" && <Yetkiler />}
+      {tabKey === "3" && <DepoYetkisi />}
+      {tabKey === "4" && <AtolyeYetkisi />}
+      {tabKey === "5" && <Yetkiler />}
     </>
   );
 }
