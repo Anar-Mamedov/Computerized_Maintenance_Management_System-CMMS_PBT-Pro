609250727b985295594dcd7da8122024b9962b1a
Anar Mamedov <anar26271@gmail.com>
2025-02-07 11:35:32 +0400

    roller ekranındad amenü ve ekran yetkisi ayarlaması eklendi

--- src/pages/Yonetim/RolTanimlari/Update/components/Tablar.jsx
+++ src/pages/Yonetim/RolTanimlari/Update/components/Tablar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Radio, Divider } from "antd";
 import MainTabs from "./MainTabs/MainTabs";
 import Yetkiler from "./Yetkiler/Yetkiler";
+import MenuVeEkranYetkileri from "./MenuVeEkranYetkileri/MenuVeEkranYetkileri";
 
 import { t } from "i18next";
 
@@ -18,11 +19,13 @@ function Tablar() {
         <Radio.Button value="1">{t("hesap")}</Radio.Button>
 
         <Radio.Button value="5">{t("yetkiler")}</Radio.Button>
+        <Radio.Button value="6">{t("menuVeEkranYetkileri")}</Radio.Button>
       </Radio.Group>
       <Divider style={{ marginBottom: 10 }} />
       {tabKey === "1" && <MainTabs />}
 
       {tabKey === "5" && <Yetkiler />}
+      {tabKey === "6" && <MenuVeEkranYetkileri />}
     </>
   );
 }
