e34f8091eb05bad5d9282aa3f2569ed1040a295e
bmert02 <berkemerrt2002@gmail.com>
2025-07-18 17:09:52 +0300

    sdfg

--- src/pages/BakımVeArizaYonetimi/PeriyodikBakimlar1/Update/components/MainTabs/components/PeryotBilgileri/components/TarihVeSayac.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TarihVeSayac(props) {
-  return <div>Mamedov</div>;
-}
-
-export default TarihVeSayac;
--- src/pages/MakineEkipman/MakineTanim/Insert/components/secondTabs/components/RequisiteTable.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function RequisiteTable() {
-  return <div>RequisiteTable</div>;
-}
