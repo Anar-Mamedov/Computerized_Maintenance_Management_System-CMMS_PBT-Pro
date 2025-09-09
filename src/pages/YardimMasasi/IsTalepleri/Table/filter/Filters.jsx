92f1df889004e9f27e67c5b346013579cc44feab
Anar Mamedov <45544776+Anar-Mamedov@users.noreply.github.com>
2025-09-10 01:31:38 +0400

    Update Turkish translations and enhance Filters component with user status functionality
    - Added new Turkish translations for user status and approval status in translation.json to improve localization.
    - Integrated UserStatusFilter in the Filters component to allow filtering based on user status, enhancing user experience.
    - Updated Filters component state management to accommodate new filter criteria, improving overall functionality.

--- src/pages/YardimMasasi/IsTalepleri/Table/filter/Filters.jsx
+++ src/pages/YardimMasasi/IsTalepleri/Table/filter/Filters.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ConditionFilter from "./ConditionFilter";
-import LocationFilter from "./LocationFilter";
-import TypeFilter from "./TypeFilter";
 import CustomFilter from "./custom-filter/CustomFilter";
 import ZamanAraligi from "./ZamanAraligi";
+import UserStatusFilter from "./UserStatusFilter";
 
 export default function Filters({ onChange }) {
   const [filters, setFilters] = React.useState({
     lokasyonlar: {},
     isemritipleri: {},
     durumlar: {},
+    onayDurumlari: {},
     customfilters: {},
   });
 
@@ -21,9 +22,14 @@ export default function Filters({ onChange }) {
     <>
       {/* <TypeFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, isemritipleri: newFilters }))} /> */}
       <ConditionFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, durumlar: newFilters }))} />
+      <UserStatusFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, onayDurumlari: newFilters }))} />
       <ZamanAraligi />
       {/* <LocationFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, lokasyonlar: newFilters }))} /> */}
       <CustomFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, customfilters: newFilters }))} />
     </>
   );
 }
+
+Filters.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
