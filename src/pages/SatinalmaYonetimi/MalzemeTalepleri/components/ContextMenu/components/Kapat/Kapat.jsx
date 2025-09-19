e8d3a5205a5de39c461624e601159c33a1001373
bmert02 <berkemerrt2002@gmail.com>
2025-09-19 12:21:11 +0300

    Siparişe Aktar Düzenleme

--- src/pages/SatinalmaYonetimi/MalzemeTalepleri/components/ContextMenu/components/Kapat/Kapat.jsx
+++ src/pages/SatinalmaYonetimi/MalzemeTalepleri/components/ContextMenu/components/Kapat/Kapat.jsx
@@ -6,7 +6,10 @@ import { DeleteOutlined, QuestionCircleOutlined } from "@ant-design/icons";
 export default function Kapat({ selectedRows, refreshTableData, disabled, hidePopover }) {
   const buttonStyle = disabled ? { display: "none" } : {};
 
-  const handleKapat = async () => {
+  const durumID = selectedRows?.[0]?.SFS_TALEP_DURUM_ID;
+  const isAc = durumID === 5 || durumID === 6;
+
+  const handleClick = async () => {
     let isError = false;
 
     for (const row of selectedRows) {
@@ -40,14 +43,17 @@ export default function Kapat({ selectedRows, refreshTableData, disabled, hidePo
   return (
   <div style={buttonStyle}>
     <Popconfirm
-      title="Talebi Kapat"
-      onConfirm={handleKapat}
-      icon={<QuestionCircleOutlined style={{ color: "red" }} />}
-    >
-      <Button style={{ paddingLeft: "0px" }} type="link" danger>
-        Talebi Kapat
-      </Button>
-    </Popconfirm>
-  </div>
-);
+      title={isAc ? "Talebi Aç" : "Talebi Kapat"}
+      onConfirm={handleClick}
+      icon={<QuestionCircleOutlined style={{ color: isAc ? "green" : "red" }} />}
+      >
+        <Button
+          style={{ paddingLeft: "0px", color: isAc ? "green" : "red" }}
+          type="link"
+        >
+          {isAc ? "Talebi Aç" : "Talebi Kapat"}
+        </Button>
+      </Popconfirm>
+    </div>
+  );
 }
